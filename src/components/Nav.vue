30787a2fc9127f906636c8bda082a0bfa69bf1ee
Travis Northcutt <travis@travisnorthcutt.com>
2020-09-08 10:47:14 -0600

    fun styles

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <nav class="bg-gray-800">
+    <nav class="bg-gradient-to-r from-yellow-200 to-orange-400">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
             <div class="hidden md:block">
-              <div class="ml-10 text-gray-300 flex items-baseline space-x-4">
+              <div class="ml-10 text-gray-900 flex items-baseline space-x-4">
                 <g-link
                   to="/"
                   class="px-3 py-2 rounded-md text-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
@@ -37,7 +37,7 @@
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
             <button
-              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
               @click="menu = !menu"
             >
               <!-- Menu open: "hidden", Menu closed: "block" -->
@@ -87,22 +87,22 @@
           >
           <g-link
             to="/articles"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
             >Articles</g-link
           >
           <g-link
             to="/hire-me"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
             >Work with me</g-link
           >
           <g-link
             to="/now"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
             >/now</g-link
           >
           <g-link
             to="/open-source-contributions"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
             >Contributions</g-link
           >
         </div>
