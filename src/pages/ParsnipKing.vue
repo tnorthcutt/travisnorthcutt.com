c4ba32146fd5d1db922878c94cc5ef95901ded60
Travis Northcutt <travis@travisnorthcutt.com>
2020-12-28 11:24:16 -0600

    make it happen

--- src/pages/ParsnipKing.vue
+++ src/pages/ParsnipKing.vue
@@ -1,1 +1,19 @@
-Hi Ben
+<template>
+  <Layout :title="title">
+    <h1>Par$nips</h1>
+    <h2>
+      All your financial problems will be solved
+    </h2>
+    <img src="https://gifs.travisnorthcutt.com/parsnips-dancing.gif" />
+
+    <CommitHistory path="src/pages/ParsnipKing.vue" />
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Par$nips",
+  },
+};
+</script>
