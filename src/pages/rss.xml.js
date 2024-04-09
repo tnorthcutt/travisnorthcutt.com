26b4dc647832a06bc1c8c30615d706e3178e25a8
Travis Northcutt <travis@travisnorthcutt.com>
2024-04-09 13:20:01 -0600

    tweak rss feed and make discoverable

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = (await getCollection("blog"))
+    .filter(p => p.data.draft !== true)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map(post => ({
+      ...post.data,
+      link: `/${post.slug}/`,
+    })),
+  });
 }
