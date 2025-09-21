c3fb0b63911a95c8f66434676cabbb4daefed6e5
Travis Northcutt <travis@travisnorthcutt.com>
2025-09-21 14:02:05 -0600

    ayy lmao essay

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -42,6 +42,25 @@ declare module 'astro:content' {
 		ContentEntryMap[C]
 	>['slug'];
 
+	export type ReferenceDataEntry<
+		C extends CollectionKey,
+		E extends keyof DataEntryMap[C] = string,
+	> = {
+		collection: C;
+		id: E;
+	};
+	export type ReferenceContentEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {}) = string,
+	> = {
+		collection: C;
+		slug: E;
+	};
+	export type ReferenceLiveEntry<C extends keyof LiveContentConfig['collections']> = {
+		collection: C;
+		id: string;
+	};
+
 	/** @deprecated Use `getEntry` instead. */
 	export function getEntryBySlug<
 		C extends keyof ContentEntryMap,
@@ -69,22 +88,27 @@ declare module 'astro:content' {
 		filter?: (entry: CollectionEntry<C>) => unknown,
 	): Promise<CollectionEntry<C>[]>;
 
+	export function getLiveCollection<C extends keyof LiveContentConfig['collections']>(
+		collection: C,
+		filter?: LiveLoaderCollectionFilterType<C>,
+	): Promise<
+		import('astro').LiveDataCollectionResult<LiveLoaderDataType<C>, LiveLoaderErrorType<C>>
+	>;
+
 	export function getEntry<
 		C extends keyof ContentEntryMap,
 		E extends ValidContentEntrySlug<C> | (string & {}),
-	>(entry: {
-		collection: C;
-		slug: E;
-	}): E extends ValidContentEntrySlug<C>
+	>(
+		entry: ReferenceContentEntry<C, E>,
+	): E extends ValidContentEntrySlug<C>
 		? Promise<CollectionEntry<C>>
 		: Promise<CollectionEntry<C> | undefined>;
 	export function getEntry<
 		C extends keyof DataEntryMap,
 		E extends keyof DataEntryMap[C] | (string & {}),
-	>(entry: {
-		collection: C;
-		id: E;
-	}): E extends keyof DataEntryMap[C]
+	>(
+		entry: ReferenceDataEntry<C, E>,
+	): E extends keyof DataEntryMap[C]
 		? Promise<DataEntryMap[C][E]>
 		: Promise<CollectionEntry<C> | undefined>;
 	export function getEntry<
@@ -103,21 +127,21 @@ declare module 'astro:content' {
 		collection: C,
 		id: E,
 	): E extends keyof DataEntryMap[C]
-		? Promise<DataEntryMap[C][E]>
+		? string extends keyof DataEntryMap[C]
+			? Promise<DataEntryMap[C][E]> | undefined
+			: Promise<DataEntryMap[C][E]>
 		: Promise<CollectionEntry<C> | undefined>;
+	export function getLiveEntry<C extends keyof LiveContentConfig['collections']>(
+		collection: C,
+		filter: string | LiveLoaderEntryFilterType<C>,
+	): Promise<import('astro').LiveDataEntryResult<LiveLoaderDataType<C>, LiveLoaderErrorType<C>>>;
 
 	/** Resolve an array of entry references from the same collection */
 	export function getEntries<C extends keyof ContentEntryMap>(
-		entries: {
-			collection: C;
-			slug: ValidContentEntrySlug<C>;
-		}[],
+		entries: ReferenceContentEntry<C, ValidContentEntrySlug<C>>[],
 	): Promise<CollectionEntry<C>[]>;
 	export function getEntries<C extends keyof DataEntryMap>(
-		entries: {
-			collection: C;
-			id: keyof DataEntryMap[C];
-		}[],
+		entries: ReferenceDataEntry<C, keyof DataEntryMap[C]>[],
 	): Promise<CollectionEntry<C>[]>;
 
 	export function render<C extends keyof AnyEntryMap>(
@@ -129,14 +153,8 @@ declare module 'astro:content' {
 	): import('astro/zod').ZodEffects<
 		import('astro/zod').ZodString,
 		C extends keyof ContentEntryMap
-			? {
-					collection: C;
-					slug: ValidContentEntrySlug<C>;
-				}
-			: {
-					collection: C;
-					id: keyof DataEntryMap[C];
-				}
+			? ReferenceContentEntry<C, ValidContentEntrySlug<C>>
+			: ReferenceDataEntry<C, keyof DataEntryMap[C]>
 	>;
 	// Allow generic `string` to avoid excessive type errors in the config
 	// if `dev` is not running to update as you edit.
@@ -151,221 +169,58 @@ declare module 'astro:content' {
 	>;
 
 	type ContentEntryMap = {
-		"blog": {
-"advice-new-freelancer.mdx": {
-	id: "advice-new-freelancer.mdx";
-  slug: "advice-new-freelancer";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"ambition.md": {
-	id: "ambition.md";
-  slug: "ambition";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"bad-design.mdx": {
-	id: "bad-design.mdx";
-  slug: "bad-design";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"books.md": {
-	id: "books.md";
-  slug: "books";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"certainty.md": {
-	id: "certainty.md";
-  slug: "certainty";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"converting-laravel-spark-navigation-to-tailwind-css.mdx": {
-	id: "converting-laravel-spark-navigation-to-tailwind-css.mdx";
-  slug: "converting-laravel-spark-navigation-to-tailwind-css";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"fallibility-of-language.md": {
-	id: "fallibility-of-language.md";
-  slug: "fallibility-of-language";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"git-clean.mdx": {
-	id: "git-clean.mdx";
-  slug: "git-clean";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"git-wip.mdx": {
-	id: "git-wip.mdx";
-  slug: "git-wip";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"handholds.md": {
-	id: "handholds.md";
-  slug: "handholds";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"happiness-is-an-extremely-uneventful-subject.md": {
-	id: "happiness-is-an-extremely-uneventful-subject.md";
-  slug: "happiness-is-an-extremely-uneventful-subject";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"horses.md": {
-	id: "horses.md";
-  slug: "horses";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"how-to-ask-for-help.md": {
-	id: "how-to-ask-for-help.md";
-  slug: "how-to-ask-for-help";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"limit-user-tracking-in-a-laravel-spark-application.mdx": {
-	id: "limit-user-tracking-in-a-laravel-spark-application.mdx";
-  slug: "limit-user-tracking-in-a-laravel-spark-application";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"love-is-a-verb.md": {
-	id: "love-is-a-verb.md";
-  slug: "love-is-a-verb";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"making-axios-work-with-external-resources-in-laravel-spark-apps.mdx": {
-	id: "making-axios-work-with-external-resources-in-laravel-spark-apps.mdx";
-  slug: "making-axios-work-with-external-resources-in-laravel-spark-apps";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"self-deception-love-grace.mdx": {
-	id: "self-deception-love-grace.mdx";
-  slug: "self-deception-love-grace";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".mdx"] };
-"show-commit-history-on-posts.md": {
-	id: "show-commit-history-on-posts.md";
-  slug: "show-commit-history-on-posts";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"sine-waves-as-representations-of-emotional-stability-and-state.md": {
-	id: "sine-waves-as-representations-of-emotional-stability-and-state.md";
-  slug: "sine-waves-as-representations-of-emotional-stability-and-state";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"the-process-is-the-point.md": {
-	id: "the-process-is-the-point.md";
-  slug: "the-process-is-the-point";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"the-sacred-and-mundane.md": {
-	id: "the-sacred-and-mundane.md";
-  slug: "the-sacred-and-mundane";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"this-is-all-there-is-maybe.md": {
-	id: "this-is-all-there-is-maybe.md";
-  slug: "this-is-all-there-is-maybe";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"what-if.md": {
-	id: "what-if.md";
-  slug: "what-if";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-};
-"til": {
-"atuin-zsh-autosuggestions-integration.md": {
-	id: "atuin-zsh-autosuggestions-integration.md";
-  slug: "atuin-zsh-autosuggestions-integration";
-  body: string;
-  collection: "til";
-  data: InferEntrySchema<"til">
-} & { render(): Render[".md"] };
-"git-diff-exclude.md": {
-	id: "git-diff-exclude.md";
-  slug: "git-diff-exclude";
-  body: string;
-  collection: "til";
-  data: InferEntrySchema<"til">
-} & { render(): Render[".md"] };
-"mysql-ansi-quotes.md": {
-	id: "mysql-ansi-quotes.md";
-  slug: "mysql-ansi-quotes";
-  body: string;
-  collection: "til";
-  data: InferEntrySchema<"til">
-} & { render(): Render[".md"] };
-"php-constructor-property-promotion.md": {
-	id: "php-constructor-property-promotion.md";
-  slug: "php-constructor-property-promotion";
-  body: string;
-  collection: "til";
-  data: InferEntrySchema<"til">
-} & { render(): Render[".md"] };
-"phpstorm-find-results-shown.md": {
-	id: "phpstorm-find-results-shown.md";
-  slug: "phpstorm-find-results-shown";
-  body: string;
-  collection: "til";
-  data: InferEntrySchema<"til">
-} & { render(): Render[".md"] };
-"ssh-term-backspace.md": {
-	id: "ssh-term-backspace.md";
-  slug: "ssh-term-backspace";
-  body: string;
-  collection: "til";
-  data: InferEntrySchema<"til">
-} & { render(): Render[".md"] };
-};
-
+		
 	};
 
 	type DataEntryMap = {
-		
+		"blog": Record<string, {
+  id: string;
+  body?: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+"til": Record<string, {
+  id: string;
+  body?: string;
+  collection: "til";
+  data: InferEntrySchema<"til">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
+	type ExtractLoaderTypes<T> = T extends import('astro/loaders').LiveLoader<
+		infer TData,
+		infer TEntryFilter,
+		infer TCollectionFilter,
+		infer TError
+	>
+		? { data: TData; entryFilter: TEntryFilter; collectionFilter: TCollectionFilter; error: TError }
+		: { data: never; entryFilter: never; collectionFilter: never; error: never };
+	type ExtractDataType<T> = ExtractLoaderTypes<T>['data'];
+	type ExtractEntryFilterType<T> = ExtractLoaderTypes<T>['entryFilter'];
+	type ExtractCollectionFilterType<T> = ExtractLoaderTypes<T>['collectionFilter'];
+	type ExtractErrorType<T> = ExtractLoaderTypes<T>['error'];
+
+	type LiveLoaderDataType<C extends keyof LiveContentConfig['collections']> =
+		LiveContentConfig['collections'][C]['schema'] extends undefined
+			? ExtractDataType<LiveContentConfig['collections'][C]['loader']>
+			: import('astro/zod').infer<
+					Exclude<LiveContentConfig['collections'][C]['schema'], undefined>
+				>;
+	type LiveLoaderEntryFilterType<C extends keyof LiveContentConfig['collections']> =
+		ExtractEntryFilterType<LiveContentConfig['collections'][C]['loader']>;
+	type LiveLoaderCollectionFilterType<C extends keyof LiveContentConfig['collections']> =
+		ExtractCollectionFilterType<LiveContentConfig['collections'][C]['loader']>;
+	type LiveLoaderErrorType<C extends keyof LiveContentConfig['collections']> = ExtractErrorType<
+		LiveContentConfig['collections'][C]['loader']
+	>;
+
 	export type ContentConfig = typeof import("../src/content/config.js");
+	export type LiveContentConfig = never;
 }
---
+++ src/content/blog/ayy-lmao.md
@@ -0,0 +1,19 @@
+---
+title: ayy lmao
+pubDate: 2025-09-21
+description: ""
+---
+
+This is a tribute, reflection, exploration, whatever, of my favorite meme:
+
+![ayy lmao meme](/images/ayy-lmao.jpg)
+
+A [link to the original tweet](https://x.com/visakanv/status/1443196315970670598).
+
+I fucking love this meme. It's been endlessly applicable in my life. I've found that many, many things in my life are easier to accept, and even more than that, find joy and beauty in, through this framing. It's an invitation to remember that two things can be true, that despite difficulties there can be beauty, even in the suffering and pain. A way to accept impermanence, and in fact to be held by impermanence, when things are incredibly, deeply hard.
+
+It's possible that part of this is because I've had ongoing struggles with depression for quite some time, and especially that "hey, things are impermanent" reminder is incredibly useful, and in fact crucial to my survival, at times. A reminder that despair comes, and then it goes again. And it may return, but even in the midst of the dark torrent of despair, there are flowers being watered, and those flowers will bloom, and they want to be witnessed.
+
+And even if that part doesn't resonate with you, if you haven't had those same struggles (which I hope you haven't!), I think this can still be useful and helpful and nourishing. It's an invitation to find beauty even in mourning. My son is fourteen, now, and I'm devastated at how quickly he's going (gone?) from a little boy to a young man. And yet, at the same time, I rejoice at who he is becoming and the interesting and full life he has before him, and at the immense blessing it will be to know and love him as an adult. Both my grief and the fading of my little boy and my fullness of heart at his growing into a man can be, and are, true.
+
+ayy lmao, indeed
